ce3c8353c5daec131e360e540932258b769c7b7b
unknown <qja277@naver.com>
2024-08-13 21:19:11 +0900

    🚀 데타 스페이스 배포를 위한 준비
    DB 테이블 생성 로직 추가

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ import sqlite3
 con = sqlite3.connect('db.db', check_same_thread=False)
 cur = con.cursor()
 
+# IF NOT EXISTS 문법은 해당 테이블이 없을 때만 생성하는 SQL문이다.
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+	            id INTEGER PRIMARY KEY,
+	            title TEXT NOT NULL,
+	            image BLOB,
+	            price INTEGER NOT NULL,
+	            description TEXT,
+	            place TEXT NOT NULL,
+	            insertAt INTEGE NOT NULL
+            );
+            """)
+
 app = FastAPI()
 
 @app.post('/items')
